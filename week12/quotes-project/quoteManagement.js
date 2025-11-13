29a7b76883eb67688b6cce9c86a9e48e8485044a
Kanokpon Suebyam <kanokpon6438@gmail.com>
2025-11-13 13:02:28 +0700

    edit week12

--- week12/quotes-project/quoteManagement.js
+++ week12/quotes-project/quoteManagement.js
@@ -1,17 +1,43 @@
 //CRUD on quotes
-import { getItems } from "./js/myLib/fetchUils.js"
-
+import { getItems, deleteItem, addItem, editItem } from "./js/myLib/fetchUils.js"
+const quoteURL = `${import.meta.env.VITE_APP_URL}/quotes`
 //GET Quotes
 async function loadQuotes() {
   try {
-    const quotes = await getItems(`${import.meta.env.VITE_APP_URL}/quotes`)
+    const quotes = await getItems(quoteURL)
     console.log(quotes)
     return quotes
   } catch (error) {
-    alert(error)
+    alert(`Quote: ${error}`)
   }
 }
-export { loadQuotes }
+async function addQuote(item) {
+  try {
+    const addedQuote = await addItem(quoteURL, item)
+    return addedQuote
+  } catch (error) {
+    alert(`Quote: ${error}`)
+  }
+}
+
+async function editQuote(item) {
+  try {
+    const editedQuote = await editItem(quoteURL, item)
+    return editedQuote
+  } catch (error) {
+    alert(`Quote: ${error}`)
+  }
+}
+
+async function deleteQuote(id) {
+  try {
+    const removeId = await deleteItem(quoteURL, id)
+    return removeId
+  } catch (error) {
+    alert(`Quote: ${error}`)
+  }
+}
+export { loadQuotes, deleteQuote, addQuote, editQuote }
 //Create Quote
 //Edit Quote
 //Delete Quote
